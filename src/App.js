81d34ee045a7de624577f6f29958b9d3f642254d
JuliValencia <juliana.valencia1@gmail.com>
2023-04-25 12:31:12 +0200

    Created React Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <div className="row">
+            <div className="col-md-6">
+              <form>
+                <input
+                  type="search"
+                  className="form-control"
+                  placeholder="Search City"
+                  autocomplete="off"
+                  autofocus="on"
+                />
+                <input
+                  type="submit"
+                  value="🔍"
+                  className="btn btn-outline-primary"
+                />
+              </form>
+            </div>
+
+            <div className="col-md-6">
+              <div className="location-date-time">
+                <h1>
+                  <strong>Lokeren</strong>,<strong> Belgium </strong>
+                </h1>
+                <p className="current-date-time">Sunday, April 2</p>
+              </div>
+            </div>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-3">
+              <ul className="current-low-high">
+                <li className="weather-forecast-temperature-low">
+                  L:<span>8</span>°
+                </li>
+                <li className="weather-forecast-temperature-high">
+                  H:<span>10</span>°
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <div className="current-temperature">
+                <strong>9</strong>
+                <span className="units">
+                  <a href="/" className="active">
+                    °C
+                  </a>{" "}
+                  |<a href="#">°F</a>
+                </span>
+                <span>
+                  <img
+                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                    className="weather-icon"
+                    alt="weather-icon"
+                  />
+                </span>
+              </div>
+              <div className="current-weather">Partly Cloudy</div>
+            </div>
+            <div className="col-3">
+              <ul className="current-humidity-wind">
+                <li>
+                  Humidity: <span>98</span>%
+                </li>
+                <li>
+                  Wind: <span>20</span>km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <hr />
+          <div className="weather-forecast"></div>
+        </div>
+        <small>
+          Coded by Juliana Valencia,
+          <a
+            href="https://github.com/JuliValencia/My-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
-
-export default App;
